eda25f152377ce243d0d5eb633acdb9d00d613d0
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-16 19:40:58 +0530

    Update Table sum.java

--- Table sum.java
+++ Table sum.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
-        System.out.println("Enter a number");
+        System.out.print("Enter a number:");
         Scanner sn=new Scanner(System.in);
         int n=sn.nextInt();
             int sum=0;
         int i=1;
         while (i <=10){
             
-            int pro=i*n;
-            System.out.println(pro);
-            
+          int pro=i*n;
+            System.out.println(n+"x"+i+"="+i*n);
             sum+=pro;
+        
             i++;
         }
-        System.out.println(sum);
+       System.out.println("Sum of Table is:"+sum);
     }
     
 }

c8c9483bd96911ba2fbe3375a78d82396aa7a8b8
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-19 17:43:37 +0530

    Update Table sum.java

--- Table sum.java
+++ Table sum.java
@@ -1,20 +1,25 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importing Scanner class for user input
+
+// Main class
 class Main {
     public static void main(String[] args) {
-        System.out.print("Enter a number:");
-        Scanner sn=new Scanner(System.in);
-        int n=sn.nextInt();
-            int sum=0;
-        int i=1;
-        while (i <=10){
-            
-          int pro=i*n;
-            System.out.println(n+"x"+i+"="+i*n);
-            sum+=pro;
+        System.out.print("Enter a number:"); // Prompting user to input a number
+        Scanner sn = new Scanner(System.in); // Creating a Scanner object to read input
+        int n = sn.nextInt(); // Reading the integer input from user
         
-            i++;
+        int sum = 0; // Initializing variable to store the sum of products
+        int i = 1; // Initializing counter variable
+        
+        // Loop to print the multiplication table and calculate the sum
+        while (i <= 10) {
+            int pro = i * n; // Calculating the product of the current number and input
+            System.out.println(n + "x" + i + "=" + pro); // Printing the current multiplication result
+            sum += pro; // Adding the product to the sum
+            
+            i++; // Incrementing the counter
         }
-       System.out.println("Sum of Table is:"+sum);
+        
+        // Printing the total sum of the multiplication table
+        System.out.println("Sum of Table is: " + sum);
     }
-    
 }

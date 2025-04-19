2325047b381893453c17508f3acdc7070f9b43fc
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-19 17:42:15 +0530

    Update No. Table.java

--- No. Table.java
+++ No. Table.java
@@ -1,20 +1,21 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importing Scanner class for user input
+
+// Main class
 class Main {
     public static void main(String[] args) {
-        System.out.print("Enter a number:");
-        Scanner sn=new Scanner(System.in);
-        int n=sn.nextInt();
-            
-        int i=1;
-        while (i <=10){
-            
-          
-            System.out.println(n+"x"+i+"="+i*n);
-            
+        System.out.print("Enter a number:"); // Prompting user to input a number
         
-            i++;
+        Scanner sn = new Scanner(System.in); // Creating a Scanner object to read input
+        int n = sn.nextInt(); // Reading the integer input from user
+        
+        int i = 1; // Initializing counter variable
+        
+        // Loop to print the multiplication table of the number
+        while (i <= 10) {
+            // Printing the multiplication result for the current value of i
+            System.out.println(n + "x" + i + "=" + i * n);
+            
+            i++; // Incrementing the counter
         }
-     
     }
-    
 }

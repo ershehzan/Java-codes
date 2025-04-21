05a3d1f355b2059b24dd8993f8a7e455cf717006
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-21 19:03:29 +0530

    Create Tri-pattern 1.java

---
+++ Tri-pattern 1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] s) {
+        
+        System.out.print("Enter the Number:");
+        Scanner sn=new Scanner(System.in);
+        
+        int n= sn.nextInt();
+        int temp=1;
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=i; j<n; j++)
+            {
+                System.out.print(" ");
+            }
+            
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(temp+" ");
+                temp++;
+            }
+            
+            System.out.println();
+        }
+    }
+}

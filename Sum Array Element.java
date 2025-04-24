8b31fce8e419e3fc5a5bf979fa583f17803a2e7d
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-24 12:55:48 +0530

    Create Sum Array Element.java

---
+++ Sum Array Element.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        // System.out.print("Enter the number: "); 
+        // int n = scanner.nextInt(); 
+
+int []arr={1,2,3,4,5,6,7,8,9,11};
+int n=10;
+int sum=0;
+
+for(int i=0;i<n;i++){
+   sum+=arr[i];
+}
+System.out.print("Sum of Array Element is: "+sum); 
+        
+        
+//  System.out.print("Reversed Array is: "); 
+//         for (int i = 0; i < n; i++) {
+//             System.out.print(arr[i]+" "); 
+//         }
+    }
+}

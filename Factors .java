db4445f421dd8a0a3e30c493e9817fbea4bfc724
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-24 18:28:24 +0530

    Update Factors .java

--- Factors .java
+++ Factors .java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
-public class PowerSeries {
+// Public class named PowerSeries
+public class Main {
     public static void main(String[] args) {
+        // Create a Scanner object to take input from the user
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter the value: ");
-        int n = scanner.nextInt();
-        
-    
 
+        // Prompt the user to enter a value
+        System.out.print("Enter the value: ");
+        
+        // Read the integer input from the user
+        int n = scanner.nextInt();
+
+        // Loop from 1 to the entered number (inclusive)
         for (int i = 1; i <= n; i++) {
-            
-            if(n%i==0){
-                  System.out.print(i+" ");
+            // Check if the current number 'i' is a factor of 'n'
+            if (n % i == 0) {
+                // Print the factor
+                System.out.print(i + " ");
             }
-            
-            
-            
         }
     }
 }

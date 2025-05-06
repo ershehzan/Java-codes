f7d45e5107e984a0c24cb3a9786f748ea7055654
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-06 19:07:31 +0530

    Create Prime using Func.java

---
+++ Prime using Func.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    // Function to check if a number is prime
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+}

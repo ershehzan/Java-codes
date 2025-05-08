c5eeb78ad02852004516d7642daf9d84e4507429
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-08 13:12:04 +0530

    Create Reverse a Str.java

---
+++ Reverse a Str.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+    //     System.out.println();
+   //  Scanner s=new Scanner(System.in);
+    String str = "PIEMR"; 
+    System.out.print("String after reversed:");
+    for (int i = str.length() - 1; i >= 0; i--) {
+        System.out.print(str.charAt(i));  
+    }
+    
+    System.out.println();  
+    }
+}

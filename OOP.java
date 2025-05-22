5a6f3215d4e20c2d87ac1f9d99a1edb7284b5227
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-22 16:12:19 +0530

    Update OOP.java

--- OOP.java
+++ OOP.java
@@ -1,17 +1,32 @@
+// Definition of the Car class
 class Car {
+    // Instance variable to store the model of the car
     String model;
+
+    // Method to start the car
     void start() {
         System.out.println(model + " starts.");
     }
+
+    // Method to stop the car
     void stop() {
         System.out.println(model + " has stopped.");
     }
 }
+
+// Main class containing the entry point of the program
 public class Main {
     public static void main(String[] args) {
+        // Creating a new Car object
         Car c = new Car();
+
+        // Setting the model of the car
         c.model = "Mustng GT";
+
+        // Calling the start method
         c.start();
+
+        // Calling the stop method
         c.stop();
     }
 }

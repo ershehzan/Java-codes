d09452f9f813b3c804ed067e03acf5718dd836b3
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-07 17:32:12 +0530

    Update Insertion Sort.java

--- Insertion Sort.java
+++ Insertion Sort.java
@@ -1,30 +1,42 @@
 import java.util.Arrays;
 
+// Class to demonstrate the Insertion Sort algorithm
 public class InsertionSort {
+
+    /**
+     * Sorts the given array in ascending order using the Insertion Sort algorithm.
+     * This method modifies the input array in place.
+     *
+     * @param arr the array of integers to be sorted
+     */
     public static void insertionSort(int[] arr) {
         int n = arr.length;
 
-        // Start from the second element
+        // Traverse the array starting from the second element
         for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
+            int key = arr[i];      // Store the current element to be compared
+            int j = i - 1;         // Initialize j to the index before i
 
-            // Move elements of arr[0..i-1] that are greater than key to one position ahead
+            // Move elements of arr[0..i-1], that are greater than key,
+            // one position ahead to make space for the key
             while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+                arr[j + 1] = arr[j];   // Shift element to the right
+                j--;                   // Move to the previous element
             }
 
-            // Place the key after the element just smaller than it
+            // Insert the key into its correct position in the sorted subarray
             arr[j + 1] = key;
         }
     }
 
     public static void main(String[] args) {
+        // Example input array to be sorted
         int[] arr = {12, 11, 13, 5, 6};
 
+        // Call insertionSort to sort the array
         insertionSort(arr);
 
+        // Print the sorted array
         System.out.println("Sorted array using Insertion Sort:");
         System.out.println(Arrays.toString(arr));
     }

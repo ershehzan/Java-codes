82454df0ac6e16efb22b99a50fdf1b71a3c58b38
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-05 18:35:09 +0530

    Update Check Vowel.java

--- Check Vowel.java
+++ Check Vowel.java
@@ -10,7 +10,8 @@ class Main {
         int count = 0; // Counter for vowels
 
         // Loop through each character in the string
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) 
+        {
             char ch = str.charAt(i);
             // Check if the character is a vowel (both lower and uppercase)
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||

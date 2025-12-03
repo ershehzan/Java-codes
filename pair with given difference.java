acc4344eb4a6ad1921b79e9e34d163df4acc6132
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-12-03 18:22:10 +0530

    Update pair with given difference.java

--- pair with given difference.java
+++ pair with given difference.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 // Main class containing the program logic
 public class Main {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
